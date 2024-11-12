6c7471fc4e561acbda7ffa5f789124de79b1ed37
_CYBERx0 <101660748+0xCYBERx0@users.noreply.github.com>
2024-11-12 12:14:09 +0500

    Update settings.py

--- knowledge/settings.py
+++ knowledge/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3%d*9*y_wd3(yn0@a%ej5-*h_i%-w&d2m2!17pgm$56n=ms4a_'
+SECRET_KEY = 'django-insecure-Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
